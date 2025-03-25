c3a7debafe240aac574b71959aeca880f5add6f1
luisgustavobuenoo <161783200+luisgustavobuenoo@users.noreply.github.com>
2025-03-24 22:46:38 -0300

    editar profile
    validação e botoes de edit, incluindo mobile

--- front-end/livraria/src/components/custom/Header.tsx
+++ front-end/livraria/src/components/custom/Header.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import logoBook from "../../assets/logo-book.svg";
+import { MenuHeader } from "./MenuHeader";
 
-const Header: React.FC = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
+const Header: React.FC = () => {  
   return (
-    <header className="w-full bg-white/10 backdrop-blur-lg shadow-lg border-b border-white rounded-b-[40px]">
-      <nav className="max-w-screen-xl mx-auto flex items-center justify-between py-4 px-6">
+    <header className=" bg-white/10 backdrop-blur-lg shadow-lg border-b border-white rounded-b-[40px] gap-2.5 || flex justify-between items-center || py-4 px-6 ">
+      <nav className="flex items-center justify-between || w-full">
         {/* Logo */}
         <a href="#" className="flex items-center gap-2">
           <img src={logoBook} alt="Logo" className="h-12 w-auto" />
@@ -22,30 +19,9 @@ const Header: React.FC = () => {
             </li>
           ))}
         </ul>
-
-        {/* Mobile Menu Button */}
-        <button className="md:hidden text-black" onClick={() => setMobileMenuOpen(true)}>
-          <Bars3Icon className="h-8 w-8" />
-        </button>
       </nav>
 
-      {/* Mobile Menu */}
-      {mobileMenuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-end z-50">
-          <div className="w-64 bg-white h-full p-6 shadow-lg">
-            <button className="mb-6 text-black" onClick={() => setMobileMenuOpen(false)}>
-              <XMarkIcon className="h-8 w-8" />
-            </button>
-            <ul className="space-y-4 text-lg font-semibold text-black">
-              {["Home", "About", "Contact", ""].map((item) => (
-                <li key={item} className="transition hover:text-gray-800 cursor-pointer">
-                  {item}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      )}
+      <MenuHeader/>
     </header>
   );
 };
