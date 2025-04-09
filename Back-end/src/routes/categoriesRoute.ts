3deb3143a042f012a1bb1298c56f73ee63c1ca46
luisgustavobuenoo <161783200+luisgustavobuenoo@users.noreply.github.com>
2025-04-09 12:43:16 -0300

    arrumando as rotas
    arrumando as rotas

--- Back-end/src/routes/categoriesRoute.ts
+++ Back-end/src/routes/categoriesRoute.ts
@@ -9,7 +9,7 @@ import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.get("/categories", getCategories);
+router.get("/categories", authMiddleware, getCategories);
 
 router.post("/categories", authMiddleware, createCategory);
 
